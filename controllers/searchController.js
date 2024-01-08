f8023479c7d4b069dfc4ee9b3d3685043859a61c
Rodrigo Eduardo Rodriguez <reraitc@gmail.com>
2024-01-08 14:55:05 -0700

    [ADD] search

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -7,19 +7,52 @@ const User = require('../models/user.js');
 
 var router = express.Router();
 
-router.post('/find', async function(req, res) {
-    const sources = ['facebook', 'telmex', 'users']
-    const results = [];
-    const client = await mongodb.connect(uri);
-    const db = client.db("whoare");
-    for (const source of sources) {
-        const collection = db.collection(source);
-        const result = await db.collection(source).find({ telefono: req.body.telefono }).toArray();
-        if (result.length) {
-            results.push({[source]: result});
-        }
-    }
-    return res.send(results);
+router.post('/find', async function (req, res) {
+	const phoneSources = ['facebook', 'telmex', 'users'];
+	const humanSources = ['ife', 'facebook', 'telmex', 'users'];
+	const results = [];
+	const client = await mongodb.connect(uri);
+	const db = client.db("whoare");
+
+
+	for (const source of phoneSources) {
+		const result = await db.collection(source).find({
+			telefono: req.body.telefono
+		}).toArray();
+		if (result.length) {
+			results.push({
+				[source]: result
+			});
+		}
+	}
+
+	const nameRegex = new RegExp(req.body.nombre, 'i');
+	const lastNameRegex = new RegExp(req.body.apellidos, 'i');
+
+	for (const source of humanSources) {
+		const result = await db.collection(source).find({
+			$and: [{
+					nombre: {
+						$regex: nameRegex
+					}
+				},
+				{
+					apellidos: {
+						$regex: lastNameRegex
+					}
+				}
+			]
+		}).toArray();
+
+		if (result.length) {
+			results.push({
+				[source]: result
+			});
+		}
+	}
+
+	client.close();
+	return res.send(results);
 });
 
 module.exports = router;
